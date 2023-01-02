48fc18aee441726cbd3d60f80e7e6b6c0153acd5
atarubift <atarubift@gmail.com>
2023-01-02 15:33:47 +0900

    Initial Commit

---
+++ qrscanner sample/qrscanner_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrscanner_sampleApp.swift
+//  qrscanner sample
+//
+//  Created by Shu Urata on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct qrscanner_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
